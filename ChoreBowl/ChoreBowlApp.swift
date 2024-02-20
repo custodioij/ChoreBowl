e0000966349c0a097824696b9acef63277f7b564
Igor Custodio <custodioij@gmail.com>
2024-02-20 15:26:56 +0100

    Initial Commit

---
+++ ChoreBowl/ChoreBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreBowlApp.swift
+//  ChoreBowl
+//
+//  Created by Igor Custodio João on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChoreBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
