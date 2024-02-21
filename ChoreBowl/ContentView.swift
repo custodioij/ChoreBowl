544394eaaa08d61a1825a206b7d62badf838d99a
Igor Custodio <custodioij@gmail.com>
2024-02-21 18:30:11 +0100

    Added screen for active chore and took some screenshots.

--- ChoreBowl/ContentView.swift
+++ ChoreBowl/ContentView.swift
@@ -10,18 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var chorename: String = ""
+    @State private var activeChore: String = ""
     @FocusState private var choreFieldIsFocused: Bool
     @State private var showingPopover = false
     
 //    @State var choreList: [String] = [] // holds all chores entered
     @StateObject private var choreListManager = ChoreListManager() // Use @StateObject for choreListManager
+    @State private var showSecondScreen = false // State to control navigation
 
     
     func showRandomChore() {
         if let randomChore = choreListManager.choreList.randomElement() {
                 let alert = UIAlertController(title: "Your chore is:", message: randomChore, preferredStyle: .alert)
             // Add two buttons that do nothing
-                alert.addAction(UIAlertAction(title: "Do it", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Do it", style: .default) {_ in
+                activeChore = randomChore
+                showSecondScreen = true
+                })
                 alert.addAction(UIAlertAction(title: "Put it back", style: .default, handler: nil))
 
                 UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
@@ -52,9 +57,6 @@ struct ContentView: View {
             Button(action: showRandomChore) {
                 Image("bowl_small")
             }
-//            Image("bowl_small")
-////                .imageScale(.small)
-//                .foregroundStyle(.tint)
             
             Text("Pick a chore from the bowl")
             
@@ -65,6 +67,25 @@ struct ContentView: View {
             }
         }
         .padding()
+        .fullScreenCover(isPresented: $showSecondScreen) {
+            SecondScreen(isPresented: $showSecondScreen, activeChore: activeChore) // Present SecondScreen when showSecondScreen is true
+                }
+    }
+}
+
+struct SecondScreen: View {
+    @Binding var isPresented: Bool // Binding to control the presentation of SecondScreen
+    let activeChore: String // Define activeChore property
+    
+    var body: some View {
+        VStack {
+            Text("Your task is:")
+            Text(activeChore)
+            Text("Did you do it?")
+            Button("I did it") {
+                isPresented = false
+                }
+        }
     }
 }
 
